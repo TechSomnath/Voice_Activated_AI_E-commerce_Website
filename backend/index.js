72d5b5e11a19c54a2125d7a28312fb1630fe246b
Somnath Ghosh <sg1941627@gmail.com>
2026-02-22 01:09:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://voice-activated-ai-e-commerce-website-1.onrender.com" , "http://localhost:5174"],
  credentials:true
 }))
 
