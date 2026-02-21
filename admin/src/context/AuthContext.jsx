145ca0698572a7343303423dc39df4ff61ed5348
Somnath Ghosh <sg1941627@gmail.com>
2026-02-22 01:14:05 +0530

    Update AuthContext.jsx

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext } from "react";
 
 export const authDataContext = createContext();
 function AuthContext({ children }) {
-  let serverUrl = "http://localhost:8000";
+  let serverUrl = "https://voice-activated-ai-e-commerce-website.onrender.com";
 
   let value = {
     serverUrl,
